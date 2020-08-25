ebf41e30013cd3c2c5ba8e5cac0bee05a0768e57
Marc Güell Segarra <marcguell@gmail.com>
2020-08-25 16:41:42 +0200

    At least això mola.

--- App.js
+++ App.js
@@ -7,8 +7,58 @@
  */
 
 import React from 'react';
-import MapView, {PROVIDER_GOOGLE, MapTypes} from 'react-native-maps';
+import MapView, {PROVIDER_GOOGLE, MapTypes, UrlTile} from 'react-native-maps';
 import {StyleSheet, View} from 'react-native';
+const mapStyle = [
+  {
+    elementType: 'labels',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    elementType: 'labels.icon',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'administrative',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'poi',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'road',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'transit',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+];
 
 const App: () => React$Node = () => {
   return (
@@ -18,13 +68,23 @@ const App: () => React$Node = () => {
           style={styles.map}
           provider={PROVIDER_GOOGLE}
           initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
+            latitude: 42.6053217,
+            longitude: 0.8774010999999999,
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
           }}
-          mapType={'satellite'}
-        />
+          mapType={'standard'}
+          rotateEnabled={false}
+          pitchEnabled={false}
+          customMapStyle={mapStyle}>
+          <UrlTile
+            urlTemplate={
+              'https://api.mapbox.com/styles/v1/mguellsegarra/cke5wh6jb2jlm19lqj1uvkifu/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWd1ZWxsc2VnYXJyYSIsImEiOiJjajNpdGU4a2gwMDJqMnhvNzk3YWx5NG4zIn0.rws1hiWTdnnNMPAS1FFkZw'
+            }
+            // maximumZ={19}
+            flipY={false}
+          />
+        </MapView>
       </View>
     </>
   );
